96097d4d25f36468dbcf73ef2c00e08f44218933
Siham0Bied0Jabaar <sihambiedjabaar@gmail.com>
2024-07-02 11:50:00 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+// src/index.js
+import express from "express";
+
+const app = express();
+const port = 2001;
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
